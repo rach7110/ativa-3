36c699d26b774fb07741083736ef866a5fd64101
MakerSquare Student <students@themakersquare.com>
2013-08-07 10:43:17 -0500

    finshed through step 10

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,24 +3,32 @@ class Project < ActiveRecord::Base
 	has_many :posts
 	belongs_to :phase
 
-  def self.onboarding
-    where(phase_id: 1)
-  end
 
-  def self.creative
-    where(phase_id: 2)
-  end
+	default_scope order('name ASC')
+	scope :onboarding, -> { where(phase_id: 1) }
+	scope :creative, -> { where(phase_id: 2) }
+	scope :design, -> { where(phase_id: 3) }
+	scope :development, -> { where(phase_id: 4) }
+	scope :implementation, -> { where(phase_id: 5) }
 
-  def self.design
-    where(phase_id: 3)
-  end
+  # def self.onboarding
+  #   where(phase_id: 1)
+  # end
 
-  def self.development
-    where(phase_id: 4)
-  end
+  # def self.creative
+  #   where(phase_id: 2)
+  # end
 
-  def self.implementation
-    where(phase_id: 5)
-  end
+  # def self.design
+  #   where(phase_id: 3)
+  # end
+
+  # def self.development
+  #   where(phase_id: 4)
+  # end
+
+  # def self.implementation
+  #   where(phase_id: 5)
+  # end
 
 end
